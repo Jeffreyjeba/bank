ddbc63fb88c47c01c29773b406ac3d0b9a944bf7
jeffrey-pt7279 <jeffrey-pt7279@jeffrey-pt7279>
2024-03-12 15:09:24 +0530

    .

---
+++ src/pojo/Accounts.java
@@ -0,0 +1,51 @@
+package pojo;
+
+import bank.AccountStatus;
+import bank.Priority;
+
+public class Accounts {
+	long accountNumber;
+	long id;
+	int branchId;
+	long balance;
+	AccountStatus status;
+	Priority priority;
+	
+	public long getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getBranchId() {
+		return branchId;
+	}
+	public void setBranchId(int branchId) {
+		this.branchId = branchId;
+	}
+	public long getBalance() {
+		return balance;
+	}
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
+	public AccountStatus getStatus() {
+		return status;
+	}
+	public void setStatus(AccountStatus status) {
+		this.status = status;
+	}
+	public Priority getPriority() {
+		return priority;
+	}
+	public void setPriority(Priority priority) {
+		this.priority = priority;
+	}
+	
+}
---
+++ src/pojo/Customers.java
@@ -0,0 +1,41 @@
+package pojo;
+
+public class Customers {
+	long customerId;
+	long id;
+	long aadharNumber;
+	String panNumber;
+	String address;
+	
+	public long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getAadharNumber() {
+		return aadharNumber;
+	}
+	public void setAadharNumber(long aadharNumber) {
+		this.aadharNumber = aadharNumber;
+	}
+	public String getPanNumber() {
+		return panNumber;
+	}
+	public void setPanNumber(String panNumber) {
+		this.panNumber = panNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
